e3b794b9abbca96ed91e4914144b97db1b5a4a12
Cagatay Aydin <cagatayaydincs@gmail.com>
2022-03-20 22:18:23 +0300

    Lecturer model has been modified

--- models/lecturer.model.js
+++ models/lecturer.model.js
@@ -1,74 +1,27 @@
 const mongoose = require('mongoose');
 
 const LecturerSchema = new mongoose.Schema({
-  LecturerName: {
-    FirstName: {
-      type: String,
-      required: true,
-    },
-    LastName: {
-      type: String,
-      required: true,
-    },
+  // name and lastname will be inherited from user.
+  // dateOfAdmission will be inherited from user.createdAt
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
   },
-  DateOfAdmission: {
-    type: Date,
-    required: true,
-  },
-  Status: {
+  status: {
     type: String,
     default: 'aktif',
     enum: ['aktif', 'pasif'],
     required: true,
   },
-  Email: {
+  email: {
     type: String,
     required: true,
   },
-  Address: [{
-    AddrType: {
-      type: String,
-      required: true,
-      trim: true,
-
-    },
-    City: {
-      type: String,
-      required: true,
-    },
-    State: {
-      type: String,
-      required: true,
-    },
-    PostalCode: {
-      type: String,
-      required: true,
-    },
-    Country: {
-      type: String,
-      required: true,
-    },
-  }],
-  Contact: [{
-    ContactType: {
-      type: String,
-      required: true,
-      trim: true,
-
-    },
-    Value: {
-      type: String,
-      required: true,
-
-    },
-  }],
-  Department: {
+  // address will be inherited from user
+  // contact will be inherited from user
+  department: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Department',
-  },
-  User: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
   },
 });
 
