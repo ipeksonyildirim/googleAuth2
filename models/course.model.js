6fdee63a6d8cb5a53eab32a39e699ab338acb920
Cagatay Aydin <cagatayaydincs@gmail.com>
2022-03-20 22:44:56 +0300

    Course model has been modified

--- models/course.model.js
+++ models/course.model.js
@@ -1,52 +1,19 @@
 const mongoose = require('mongoose');
 
 const CourseSchema = new mongoose.Schema({
-  shortCode: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  credit: {
-    type: Number,
-    required: true,
-  },
+  code: String,
+  name: String,
+  credit: Number,
   department: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Department',
   },
-  termYear: {
-    type: String,
-    required: true,
-  },
-  termName: {
-    type: String,
-    required: true,
-  },
-  lessonHours: [{
-    day: {
-      type: String,
-      required: true,
-    },
-    startedHours: {
-      type: Number,
-      required: true,
-    },
-    finishedHours: {
-      type: Number,
-      required: true,
-    },
-    location: {
-      type: String,
-      required: true,
-    },
-    zoomId: {
-      type: String,
-      required: true,
-    },
-
+  terms: [String],
+  schedule: [{
+    day: String,
+    time: String,
+    location: String,
+    zoomId: String,
   }],
 
 });
