17cf5c6b85acb91c13b1d11bd538a4b298c6ec78
ipek <isonyildirim@etu.edu.tr>
2022-03-20 23:38:50 +0300

    Post model created

---
+++ models/post.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const postSchema = Schema({
+  title: {
+    type: String,
+    required: [true, "Please enter the post's title"],
+  },
+  content: {
+    type: String,
+    required: [true, "Please enter the post's content"],
+  },
+  imagePath: {
+    type: String,
+  },
+  course: {type: mongoose.Schema.ObjectId, ref: "Course", required: true },
+  createdAt: { type: Date, default: Date.now, required: true },
+  createdBy: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
+  comments: [{
+    content: String,
+    createdAt: { type: Date, default: Date.now, required: true },
+    createdBy: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
+  }],
+});
+  
+module.exports = mongoose.model('Post', postSchema);
